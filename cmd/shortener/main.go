6755f4acdf02b31aeec4b9560ff0e215acd222c3
gerasimovpavel <gerasimovpg@outlook.com>
2023-10-08 10:25:36 +0300

    fixed something

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"math/rand"
 	"net/http"
+	"os"
 	"strings"
 	"time"
 
@@ -14,8 +15,10 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 )
 
+// мапа для хранения ссылок
 var pairs = make(map[string]string)
 
+// findByValue Поиск ключа по значению пары
 func findByValue(value string) (key string, ok bool) {
 	for k, v := range pairs {
 		if v == value {
@@ -27,6 +30,7 @@ func findByValue(value string) (key string, ok bool) {
 	return
 }
 
+// genShort Рандом-генератор коротких ссылок
 func genShort() string {
 	const allowchars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	const urllen = 7
@@ -38,12 +42,16 @@ func genShort() string {
 	return string(short)
 }
 
+// postHandler handler для POST запросов
 func postHandler(w http.ResponseWriter, r *http.Request) {
+	// читаем тело запросв
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
+		// при ошибке возвращаеь 400 ошибку
 		http.Error(w, fmt.Sprintf("%s\n\nНе могу прочитать тело запроса", err.Error()), http.StatusBadRequest)
 		return
 	}
+	// Длинный URL
 	origURL := string(body)
 	if origURL == "" {
 		http.Error(w, "URL в теле не найден", http.StatusBadRequest)
@@ -57,7 +65,7 @@ func postHandler(w http.ResponseWriter, r *http.Request) {
 		pairs[shortURL] = origURL
 	}
 
-	// Создаем ссылку для ответа
+	// Создаем URL для ответа
 	tempURL := fmt.Sprintf(`%s/%s`, config.Options.ShortURLHost, shortURL)
 	w.Header().Set("Content-Type", "text/plain")
 	w.WriteHeader(http.StatusCreated)
@@ -65,21 +73,25 @@ func postHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func getHandler(w http.ResponseWriter, r *http.Request) {
+	// Определям короткую ссылку из пути
 	shortURL := strings.TrimPrefix(r.URL.Path, "/")
 	if shortURL == "" {
 		http.Error(w, "Ссылка не указана", http.StatusBadRequest)
 		return
 	}
-	origURL, ok := pairs[shortURL]
 	// получаем оригинальный урл из мапы пар
+	origURL, ok := pairs[shortURL]
+	// при ошибки возвращаем ошибку 404
 	if !ok {
 		http.Error(w, "Ссылка не найдена", http.StatusNotFound)
 		return
+	} else {
+		// иначе 307 редирект на оригинальный урл
+		http.Redirect(w, r, origURL, http.StatusTemporaryRedirect)
 	}
-	// 307 редирект на оригинальный урл
-	http.Redirect(w, r, origURL, http.StatusTemporaryRedirect)
 }
 
+// mainHandler Handler для первых инкрементов
 func mainHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodPost:
@@ -133,24 +145,42 @@ func mainHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// mainRouter роутер http запросов
 func mainRouter() chi.Router {
 	r := chi.NewRouter()
 	r.Use(middleware.RealIP, middleware.Logger, middleware.Recoverer)
 	r.Route("/", func(r chi.Router) {
+		// роут для POST
 		r.Post("/", postHandler) // POST /
 		r.Route("/{shortURL}", func(r chi.Router) {
+			// роут для GET
 			r.Get("/", getHandler) // GET /{shortURL}
 		})
 	})
 	return r
 }
 
+// main
 func main() {
-	flag.StringVarP(&config.Options.Host, "a", "a", ":8080", "Адрес HTTP-сервера")
-	flag.StringVarP(&config.Options.ShortURLHost, "b", "b", "http://localhost:8080", "URL короткой ссылки")
-
-	flag.Parse()
-
+	var ok bool
+	// ищем переменную SERVER_ADDRESS
+	config.Options.Host, ok = os.LookupEnv(`SERVER_ADDRESS`)
+	if !ok {
+		// если не нашли, обрабатываем командную строку
+		flag.StringVarP(&config.Options.Host, "a", "a", ":8080", "Адрес HTTP-сервера")
+	}
+	// ищем переменную BASE_URL
+	config.Options.ShortURLHost, ok = os.LookupEnv(`BASE_URL`)
+	if !ok {
+		// если не нашли, обрабатываем командную строку
+		flag.StringVarP(&config.Options.ShortURLHost, "b", "b", "http://localhost:8080", "URL короткой ссылки")
+	}
+	// если хотя бы одну переменную ищем в командной строке
+	if !ok {
+		// парсим аргументы
+		flag.Parse()
+	}
+	// запускаем сервер
 	err := http.ListenAndServe(config.Options.Host, mainRouter())
 	if err != nil {
 		panic(err)

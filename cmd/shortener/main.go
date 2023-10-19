1206146f54d09d491a4a091dae1bc708970c2287
gerasimovpavel <gerasimovpg@outlook.com>
2023-10-19 07:36:24 +0300

    create iter6

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"github.com/gerasimovpavel/shortener.git/internal/config"
 	"github.com/gerasimovpavel/shortener.git/internal/handlers"
+	"github.com/gerasimovpavel/shortener.git/internal/log"
+	"go.uber.org/zap"
 	"net/http"
 )
 
@@ -10,8 +12,18 @@ import (
 func main() {
 	//Парсим переменные и аргументы команднй строки
 	config.ParseEnvFlags()
+
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		panic(err)
+	}
+	defer logger.Sync()
+
+	// делаем регистратор SugaredLogger
+	log.Sugar = *logger.Sugar()
+
 	// запускаем сервер
-	err := http.ListenAndServe(config.Options.Host, handlers.MainRouter())
+	err = http.ListenAndServe(config.Options.Host, log.WithLogging(handlers.MainRouter()))
 	if err != nil {
 		panic(err)
 	}

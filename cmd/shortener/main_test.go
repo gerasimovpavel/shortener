613e53823534e7e414b5c39606b08242ec25bf89
gerasimovpavel <gerasimovpg@outlook.com>
2023-11-08 13:47:19 +0300

    fixed iteration2 bugs

--- cmd/shortener/main_test.go
+++ cmd/shortener/main_test.go
@@ -16,23 +16,26 @@ import (
 // Test_main Тест основных возможностей
 func Test_main(t *testing.T) {
 	tests := []struct {
-		name       string
-		method     string
-		origURL    string
-		wantStatus int
+		name         string
+		method       string
+		origURL      string
+		wantStatuses []int
 	}{
 		{"Create short url",
 			http.MethodPost,
 			"https://practicum.yandex.ru",
-			http.StatusCreated},
+			[]int{http.StatusCreated, http.StatusConflict},
+		},
 		{"Get original url",
 			http.MethodGet,
 			"https://practicum.yandex.ru",
-			http.StatusTemporaryRedirect},
+			[]int{http.StatusTemporaryRedirect},
+		},
 		{"Get short url over json",
 			http.MethodPost,
 			`{"url":"https://practicum.yandex.ru"}`,
-			http.StatusCreated},
+			[]int{http.StatusCreated, http.StatusConflict},
+		},
 	}
 	err := storage.NewStorage()
 	if err != nil {
@@ -115,7 +118,13 @@ func Test_main(t *testing.T) {
 					}
 				}
 				res.Body.Close()
-				if res.StatusCode != tt.wantStatus {
+				var i int64
+				for _, s := range tt.wantStatuses {
+					if s == res.StatusCode {
+						i++
+					}
+				}
+				if i == 0 {
 					panic("Не смог создать короткий URL")
 				}
 			}

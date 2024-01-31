c8fdba2851dfe3c282de12088eb2a640e46c9c73
gerasimovpavel <gerasimovpg@outlook.com>
2024-01-31 11:43:35 +0300

    почему-то в автотесте 14 убрали авторизацию по куки

--- internal/router/router.go
+++ internal/router/router.go
@@ -24,16 +24,15 @@ func MainRouter() chi.Router {
 		r.Get("/{shortURL}", handlers.GetHandler)
 		r.Get("/ping", handlers.PingHandler)
 		r.Group(func(r chi.Router) {
+			r.Use(
+				mw.AuthHeader,
+			)
 			r.Post("/", handlers.PostHandler)
 			r.Route("/api", func(r chi.Router) {
 				r.Route("/shorten", func(r chi.Router) {
 					r.Post("/", handlers.PostJSONHandler)
 					r.Post("/batch", handlers.PostJSONBatchHandler)
 				})
-
-				r.Use(
-					mw.AuthHeader,
-				)
 				r.Route("/user", func(r chi.Router) {
 					r.Get("/urls", handlers.GetUserURLHandler)
 					r.Delete("/urls", handlers.DeleteUserURLHandler)
